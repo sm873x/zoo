cd73c1164fcfa05866795eee77b36c4375bfb410
Stella Ma <sm873x@gmail.com>
2016-07-25 16:14:14 -0400

    Refactored test for animal name and date of birth. Added method on animal function for age.

--- build/js/main.js
+++ build/js/main.js
@@ -3,9 +3,21 @@
 
     window.zoo = ns = ( ns || {} );
 
-    ns.Animal = function Animal() {
-        console.log('Animal constructor fn');
+    ns.Animal = function Animal(name) {
+        // console.log('Animal constructor fn');
+        this.name = name;
+        this.dateOfBirth = new Date();
     };
+    ns.Animal.prototype.age = function() {
+        var now = new Date();
+        var age = now - this.dateOfBirth;
+        return age;
+    };
+
+    // ns.Species = function Species(name, dateOfBirth) {
+    //     Atom.call(this, name, dateOfBirth);
+    //
+    // };
 })(window.zoo);
 
 //# sourceMappingURL=main.js.map
--- test/specs/test_index.js
+++ test/specs/test_index.js
@@ -4,18 +4,29 @@
     var assert = window.chai.assert;
 
     suite('Animal constructor function', function() {
-
         test('ensure Animal constructor function exists', function() {
             assert.ok(window.zoo.Animal, 'Animal function exists');
             assert.strictEqual(typeof(window.zoo.Animal), 'function', 'Animal is a function' );
+            console.log(window.zoo.Animal);
         });
 
-        // test('Animals have a name and DOB', function() {
-        //
-        // });
-        //
+        test('Animals have a name and date of birth', function() {
+            var fred = new window.zoo.Animal('fred');
+            var date = new Date();
+            console.log(fred);
+            assert.strictEqual(fred.name, 'fred', 'Animal has name');
+            assert.strictEqual(fred.dateOfBirth.getYear(), date.getYear(), 'animal birth year matches');
+            assert.strictEqual(fred.dateOfBirth.getMonth(), date.getMonth(), 'animal birth month matches');
+            assert.strictEqual(fred.dateOfBirth.getDate(), date.getDate(), 'animal birth date matches');
+        });
+
         // test('Animals have method for determining age in years', function() {
+        //     var now = Date.now();
         //
         // });
+    });
+
+    suite('species function', function(){
+
     });
 })();
